05cb9ff96f08af3f1875545a430cd4e17ac6d1e9
unknown <Ataiborinbaev@gmail.com>
2022-08-20 17:28:40 +0600

    dz1

---
+++ src/pages/AbotUs/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs(props) {
+  return (
+    <div>
+      <h1>title</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, enim?</p>
+    </div>
+  );
+}
+
+export default AboutUs;
---
+++ src/pages/Portfolio/Portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Portfolio(props) {
+  return (
+    <div>
+        <ul>
+          <li>test1</li>
+          <li>test2</li>
+          <li>test3</li>
+        </ul>
+    </div>
+  );
+}
+
+export default Portfolio;
